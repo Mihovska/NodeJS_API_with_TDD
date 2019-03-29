8c7fba951c5e3f3bfce9e3a6efc844d0fca21e7c
Vili <vili_mihovska@live.com>
2019-03-29 17:17:13 +0200

    unit tests setup customer API

---
+++ customer-service/customer-service/tests/unit/customer/customer.module.spec.js
@@ -0,0 +1,16 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var CustomerModule = require('../../../modules/customer/customer.module');
+
+describe('CustomerModule', function(){
+    describe('customer.module file', function(){
+        it ('should confirm CustomerModule function exists', function(){
+            expect(CustomerModule).to.be.a('function');
+        });
+
+        it('should confirm CustomerModule function returns an object', function(){
+            expect(CustomerModule()).to.be.a('object');
+        });
+    });
+});
