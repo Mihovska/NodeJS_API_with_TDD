f7b5dff34071fc70e9a43f7f8c5930cfef249f5f
VMihovska <vili_mihovska@live.com>
2019-04-09 17:34:54 +0300

    update customer failure unit test

--- customer-service/customer-service/tests/unit/customer/customer.service.spec.js
+++ customer-service/customer-service/tests/unit/customer/customer.service.spec.js
@@ -157,5 +157,22 @@ describe('CustomerService', function(){
                     expect(data).to.deep.equal(expectedModifiedCustomer);
                 })
         });
+
+        it('should throw error while updating Customer', function(){
+            expectedError = ErrorFixture.unknownError;
+            existingCustomer = CustomerFixture.createdCustomer;
+
+            CustomerModelMock.expects('findByIdAndUpdate')
+                .withArgs(existingCustomer._id, existingCustomer, {new: true})
+                .chain('exec')
+                .rejects(expectedError);
+
+            return CustomerService.updateCustomer(existingCustomer._id, existingCustomer)
+                .catch(function(error){
+                    CustomerModelMock.verify();
+
+                    expect(error).to.deep.equal(expectedError);
+                });
+        });
     });
 });
